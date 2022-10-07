e1e5b679a7a2a029317408d71bcb78dc8f482896
Gabriel Leme <gabriel.leme.1@hotmail.com>
2022-10-07 16:48:30 -0300

    Aggregation Pattern

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,9 +1,15 @@
+using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot();
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json",
+        true,
+        true);
+});
 
 builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
 {
@@ -12,10 +18,14 @@ builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
     loggingBuilder.AddDebug();
 });
 
+builder.Services.AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+await app.UseOcelot();
+
 app.Run();
 
-await app.UseOcelot();
---
+++ src/ApiGateways/Shopping.Aggregator/Services/IBasketService.cs
@@ -0,0 +1,7 @@
+﻿using Shopping.Aggregator.Models;
+
+namespace Shopping.Aggregator.Services;
+public interface IBasketService
+{
+    Task<BasketModel> GetBasket(string userName);
+}
