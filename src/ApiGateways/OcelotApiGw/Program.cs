ab7d29e312ff2cdb4ab19a651cb78a9f1e76e8d1
Gabriel Leme <gabriel.leme.1@hotmail.com>
2022-10-07 15:16:38 -0300

    Ocelot Gw

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,21 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot();
+
+builder.Host.ConfigureLogging((hostingContext, loggingBuilder) =>
+{
+    loggingBuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+await app.UseOcelot();
